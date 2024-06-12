4af863ecae15b0cbfbb5c38d347aef8827138530
RyanDjafri <ryandjafripro@gmail.com>
2024-06-12 12:10:38 +0200

    error corrected

--- front/src/containers/NewBill.js
+++ front/src/containers/NewBill.js
@@ -10,16 +10,16 @@ export default class NewBill {
     const formNewBill = this.document.querySelector(
       `form[data-testid="form-new-bill"]`
     );
-    formNewBill.addEventListener("submit", this.handleSubmit.bind(this));
+    formNewBill.addEventListener("submit", this.handleSubmit);
     const file = this.document.querySelector(`input[data-testid="file"]`);
-    file.addEventListener("change", this.handleChangeFile.bind(this));
+    file.addEventListener("change", this.handleChangeFile);
     this.fileUrl = null;
     this.fileName = null;
     this.billId = null;
     new Logout({ document, localStorage, onNavigate });
   }
 
-  handleChangeFile = async (e) => {
+  handleChangeFile = (e) => {
     e.preventDefault();
     const fileInput = this.document.querySelector(`input[data-testid="file"]`);
     const file = fileInput.files[0];
@@ -33,12 +33,12 @@ export default class NewBill {
     this.fileName = fileName;
 
     const formData = new FormData();
-    const email = JSON.parse(JSON.parse(localStorage.getItem("user"))).email;
+    const email = JSON.parse(localStorage.getItem("user")).email;
     const allowedFileTypes = ["image/jpeg", "image/jpg", "image/png"];
 
     if (!allowedFileTypes.includes(file.type)) {
       console.error(
-        "Invalid file type. Please select a JPG, JPEG, or PNG file."
+        "Invalid file type. Please select a JPG, JPEG, PNG, or PDF file."
       );
       this.fileValidationPassed = false;
       return;
@@ -47,59 +47,65 @@ export default class NewBill {
     formData.append("file", file);
     formData.append("email", email);
 
-    try {
-      const { fileUrl, key } = await this.store.bills().create({
+    this.store
+      .bills()
+      .create({
         data: formData,
         headers: {
           noContentType: true,
         },
+      })
+      .then(({ fileName, key }) => {
+        this.fileUrl = fileName;
+        this.billId = key;
+        console.log("File uploaded successfully:", fileName);
+      })
+      .catch((error) => {
+        console.error("Error uploading file:", error);
       });
-      this.fileUrl = fileUrl;
-      this.billId = key;
-      console.log("File uploaded successfully:", fileUrl);
-    } catch (error) {
-      console.error("Error uploading file:", error);
-    }
   };
 
-  handleSubmit = async (e) => {
+  handleSubmit = (e) => {
     e.preventDefault();
 
     if (!this.fileValidationPassed) {
       console.error("File validation failed. Cannot submit the form.");
       return;
-    }
-
-    const email = JSON.parse(localStorage.getItem("user")).email;
-    const bill = {
-      email,
-      type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
-      name: e.target.querySelector(`input[data-testid="expense-name"]`).value,
-      amount: parseInt(
-        e.target.querySelector(`input[data-testid="amount"]`).value
-      ),
-      date: e.target.querySelector(`input[data-testid="datepicker"]`).value,
-      vat: e.target.querySelector(`input[data-testid="vat"]`).value,
-      pct:
-        parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) ||
-        20,
-      commentary: e.target.querySelector(`textarea[data-testid="commentary"]`)
-        .value,
-      fileUrl: this.fileUrl,
-      fileName: this.fileName,
-      status: "pending",
-    };
-
-    try {
-      await this.store.bills().create({
-        data: JSON.stringify(bill),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+    } else {
+      const email = JSON.parse(localStorage.getItem("user")).email;
+      const bill = {
+        email,
+        type: e.target.querySelector(`select[data-testid="expense-type"]`)
+          .value,
+        name: e.target.querySelector(`input[data-testid="expense-name"]`).value,
+        amount: parseInt(
+          e.target.querySelector(`input[data-testid="amount"]`).value
+        ),
+        date: e.target.querySelector(`input[data-testid="datepicker"]`).value,
+        vat: e.target.querySelector(`input[data-testid="vat"]`).value,
+        pct:
+          parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) ||
+          20,
+        commentary: e.target.querySelector(`textarea[data-testid="commentary"]`)
+          .value,
+        fileUrl: this.fileUrl,
+        fileName: this.fileName,
+        status: "pending",
+      };
+      this.updateBill(bill);
       this.onNavigate(ROUTES_PATH["Bills"]);
-    } catch (error) {
-      console.error("Error creating bill:", error);
+    }
+  };
+
+  updateBill = (bill) => {
+    if (this.store) {
+      this.store
+        .bills()
+        .update({ data: JSON.stringify(bill), selector: this.billId })
+        .then(() => {
+          this.onNavigate(ROUTES_PATH["Bills"]);
+        })
+        .catch((error) => console.error("Error updating bill:", error));
     }
   };
 }
